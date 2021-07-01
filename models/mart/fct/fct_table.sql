e2b016d0bf5dce476bac989f2507cf147847a472
daiana.vega1@baruchmail.cuny.edu <daiana.vega1@baruchmail.cuny.edu>
2021-07-01 00:21:35 +0000

    update

--- models/mart/fct/fct_table.sql
+++ models/mart/fct/fct_table.sql
@@ -2,11 +2,11 @@
 materialized='table'
 ) }}
 
-SELECT Unique_Key as recordNumber, Date as WeatherDate, Date(Created_Date) as Create_Date, Date(Closed_Date) as Close_Date, 
+SELECT Unique_Key as recordNumber, w.Date as WeatherDate, Date(Created_Date) as Create_Date, Date(Closed_Date) as Close_Date, 
 Complaint_Type, Descriptor, Status, Borough, Avg_Temp
 
  FROM `cis9440-final.311.311_Data` c 
 
 LEFT JOIN `cis9440-final.Weather.Weather_Data` w
 
-ON DATE(c.Created_Date) = w.Date 
+ON DATE(c.Created_Date) = w.Date
